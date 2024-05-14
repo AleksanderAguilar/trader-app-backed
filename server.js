f8939afb532ff134eba75556fa7bb57d16a057bc
AleksanderAguilar <aleksanderaguilart@gmail.com>
2024-05-14 00:01:14 -0400

    added CORS to allow client to connect

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express');
 const userRoutes = require('./src/users/routes');
 const app = express();
 const port = 4000;
+const cors = require('cors');
 
-app.use(express.json())
+app.use( cors({
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true // Allow credentials
+
+}));
+
+app.use(express.json());
+app.use("/api/v1/users", userRoutes);
+
+
+
 
 app.get("/", (req, res) => {
+   
     res.send("hellos world");
 });
 
 
-app.use("/api/v1/users", userRoutes);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
+
--- src/users/controller.js
+++ src/users/controller.js
@@ -5,6 +5,7 @@ const getUsers = (req, res) => {
     pool.query(queries.getUsers, (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
+        console.log('sent users')
     });
 };
 
